cfe41b9db6efb43728d93da99b37b8a77028fcd8
eusebiat@arch <eusebiat@arch>
2024-03-04 22:31:26 +0300

    feat: added all packages in arch feat: update fish greeting

--- setup.sh
+++ setup.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
-package_manager="apt install"
+declare -A osInfo;
+osInfo[/etc/redhat-release]=yum
+osInfo[/etc/arch-release]="pacman -Syu"
+osInfo[/etc/gentoo-release]=emerge
+osInfo[/etc/SuSE-release]=zypp
+osInfo[/etc/debian_version]="apt-get install"
+osInfo[/etc/alpine-release]=apk
 
-sudo $package_manager fish exa
+
+for f in ${!osInfo[@]}
+do
+    if [[ -f $f ]];then
+        package_manager=${osInfo[$f]}
+    fi
+done
+
+
+sudo $package_manager fish exa neofetch waybar inotify-tools otf-font-awesome neovim firefox kitty brightnessctl
 
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+fish
 omf install bobthefish
+fish
 set -g theme_powerline_fonts no
-cp .config/fish ~/.config
-cp .config/omf  ~/.config
+cp .config/* ~/.config/
